5874da139523774b8754a0fe4bad29cceefce972
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-06-02 21:14:15 +0800

    added file parsing

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,55 @@ use num_bigint::{BigUint, RandBigInt};
 use sha2::{Sha256, Sha512, Digest};
 use rand::rngs::OsRng;
 use ed25519_dalek::Keypair;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
-const key_length: usize = 32;
-const checksum_length: usize = 4;
+const KEY_LENGTH: usize = 32;
+const CHECKSUM_LENGTH: usize = 4;
 
-const pub_prefix: [u8; 2] = [0x5f, 0xb1];
-const priv_prefix: [u8; 2] = [0x64, 0x78];
+const PUB_PREFIX: [u8; 2] = [0x5f, 0xb1];
+const PRIV_PREFIX: [u8; 2] = [0x64, 0x78];
 
-const filepath: &str = "prefixes";
+const FILEPATH: &str = "prefixes";
+const B58_ALPHABET: &str = "123456789ABCDEFGHJKLMNPQRSTUVWXYzabcdefghijkmnopqrstuvwxyz";
 
 
 fn main() {
     let keypair = generate_ed25519_keypair();
-    let pub_address = human_readable_address(&pub_prefix, &rcd(keypair.public.to_bytes()));
-    let priv_address = human_readable_address(&priv_prefix, &keypair.secret.to_bytes());
+    let pub_address = human_readable_address(&PUB_PREFIX, &rcd(keypair.public.to_bytes()));
+    let priv_address = human_readable_address(&PRIV_PREFIX, &keypair.secret.to_bytes());
     println!("{}\n{}", pub_address, priv_address);
 
+}
+
+fn read_file() -> Vec<String> {
+    let mut prefixes = Vec::new();
+    let lines = parse_lines(FILEPATH).expect("Unable to open prefixes file");
+    for prefix in lines {
+        let word = prefix.unwrap();
+        if word.is_ascii() {
+            prefixes.push(word);
+        }
+        
+    }
+    prefixes
+}
+
+fn prefixes_as_bytes(prefixes: Vec<String>) -> Vec<Vec<u8>> {
+    let mut prefix_bytes = Vec::new(); 
+    for prefix in prefixes {
+        prefix_bytes.push(prefix.as_bytes().to_vec())
+    }
+    prefix_bytes
+}
+
+fn parse_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
 
 fn generate_256_uint()-> BigUint {
@@ -55,8 +88,8 @@ fn slice_to_array(slice: &[u8]) -> [u8; 32] {
 fn assemble_address_bytes(prefix: &[u8], raw: &[u8])-> Vec<u8> {
     let (mut key, mut output) = (Vec::new(), Vec::new());
     key.extend_from_slice(prefix);
-    key.extend_from_slice(&raw[..key_length]);
-    let checksum = &double_sha(&key)[..checksum_length];
+    key.extend_from_slice(&raw[..KEY_LENGTH]);
+    let checksum = &double_sha(&key)[..CHECKSUM_LENGTH];
     output.extend_from_slice(&key);
     output.extend_from_slice(checksum);
     output
@@ -65,8 +98,8 @@ fn assemble_address_bytes(prefix: &[u8], raw: &[u8])-> Vec<u8> {
 fn human_readable_address(prefix: &[u8], raw: &[u8])-> String {
     let (mut key, mut output) = (Vec::new(), Vec::new());
     key.extend_from_slice(prefix);
-    key.extend_from_slice(&raw[..key_length]);
-    let checksum = &double_sha(&key)[..checksum_length];
+    key.extend_from_slice(&raw[..KEY_LENGTH]);
+    let checksum = &double_sha(&key)[..CHECKSUM_LENGTH];
     output.extend_from_slice(&key);
     output.extend_from_slice(checksum);
     bs58::encode(output).into_string()
